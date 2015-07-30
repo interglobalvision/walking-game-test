9639b33f76b9fa198293cbf9ad0c6ed31c1cc24d
cas <casska@gmail.com>
2015-07-30 13:11:12 -0500

    add - accel comparison

---
+++ app/client/helpers/app.browserify.js
@@ -0,0 +1,1 @@
+DTW = require('dtw');
--- app/client/templates/accelerometer/accelerometer.js
+++ app/client/templates/accelerometer/accelerometer.js
@@ -1,88 +1,135 @@
-var accel = {
-  record: [],
-  data: {
-    x: [],
-    y: [],
-    z: [],
-  },
-  pushRecord: function(event) {
-    // _this is different here because the scope comes from window
-    var _this = accel;
+var accel = null;
 
-    _this.record.push(event);
-  },
-  plot: function() {
-    var _this = this;
-    var ctx = document.getElementById('lineChart').getContext("2d"); 
+Template.accelerometer.onRendered(function () {
+  $('.lineChart').css('width', window.innerWidth);
+  $('.lineChart').css('height', window.innerHeight * 0.8);
+});
 
-    // Plot options
-    var plotOptions = {
-      pointDot: false,
-      datasetFill: false,
-      bezierCurve: false,
-    };
+Template.accelerometer.onCreated( function () {
 
-    // Build data
-    var accelLabels = [];
-
-    for(var i = 0; i < _this.data.x.length; i++) {
-      accelLabels.push(i);
-    }
-
-    var accelData = {
-      labels: accelLabels,
-      datasets: [
-        {
-          label: "X",
-          data: _this.data.x,
-          strokeColor: 'red',
-        },
-        {
-          label: "Y",
-          data: _this.data.y,
-          strokeColor: 'blue',
-        },
-        {
-          label: "Z",
-          data: _this.data.z,
-          strokeColor: 'green',
-        },
-      ],
-    };
-
-    // Plot data
-    var accelChart =  new Chart(ctx).Line(accelData, plotOptions);
-  },
-  start: function() {
-    var _this = this;
-
-    // Clear record
-    _this.record = [];
-    _this.data = {
+  accel = {
+    record: [],
+    data: {
       x: [],
       y: [],
       z: [],
-    };
+    },
+    axis: ['x'],
+    baseData: {
+      x: [],
+      y: [],
+      z: [],
+    },
+    pushRecord: function(event) {
+      // _this is different here because the scope comes from window
+      var _this = accel;
 
-  },
-  end: function() {
-    var _this = this;
+      _this.record.push(event);
+    },
+    plot: function(ctxId) {
 
-    // Put each axis on its own array
-    for(var i = 0; i < _this.record.length; i++) {
-      _this.data.x.push(_this.record[i].accelerationIncludingGravity.x); 
-      _this.data.y.push(_this.record[i].accelerationIncludingGravity.y); 
-      _this.data.z.push(_this.record[i].accelerationIncludingGravity.z);
-    }
+      var _this = this;
+      var axis = _this.axis;
+      var ctx = document.getElementById(ctxId).getContext("2d"); 
+      var dataToPlot;
+      
+      if( ctxId === 'baseChart' ) {
+        dataToPlot = _this.baseData;
+      } else if ( ctxId === 'testChart' ) {
+        dataToPlot = _this.data;
+      }
 
-    _this.plot();
-  },
-};
+      // Plot options
+      var plotOptions = {
+        pointDot: false,
+        datasetFill: false,
+        bezierCurve: false,
+      };
 
-Template.accelerometer.onRendered(function () {
-  $('#lineChart').css('width', window.innerWidth);
-  $('#lineChart').css('height', window.innerHeight);
+      // Build data
+      var accelLabels = [];
+
+      for(var i = 0; i < dataToPlot[axis[0]].length; i++) {
+        accelLabels.push(i);
+      }
+
+      var accelData = {
+        labels: accelLabels,
+        datasets: [],
+      };
+
+      for ( var i = 0; i < axis.length; i++ ) {
+
+        accelData.datasets.push({
+          label: axis[i],
+          data: dataToPlot[axis[i]],
+          strokeColor: _this.randomColorGenerator(),
+        });
+
+      }
+
+      // Plot data
+      var accelChart =  new Chart(ctx).Line(accelData, plotOptions);
+    },
+    start: function() {
+      var _this = this;
+
+      // Clear record
+      _this.record = [];
+
+    },
+    end: function() {
+      var _this = this;
+
+      _this.data = {
+        x: [],
+        y: [],
+        z: [],
+      };
+      // Put each axis on its own array
+      for(var i = 0; i < _this.record.length; i++) {
+        for( var x = 0; x < _this.axis.length; x++ ) {
+          _this.data[_this.axis[x]].push(_this.record[i].accelerationIncludingGravity[_this.axis[x]]); 
+        }
+      }
+
+      _this.plot('testChart');
+
+    },
+    endBase: function() {
+      var _this = this;
+
+      _this.baseData = {
+        x: [],
+        y: [],
+        z: [],
+      };
+      // Put each axis on its own array
+      for(var i = 0; i < _this.record.length; i++) {
+        for( var x = 0; x < _this.axis.length; x++ ) {
+          _this.baseData[_this.axis[x]].push(_this.record[i].accelerationIncludingGravity[_this.axis[x]]); 
+        }
+      }
+
+      _this.plot('baseChart');
+
+    },
+    compare: function() {
+      var _this = this;
+      var dtw = new DTW();
+
+      $('.comparison-result span').html('');
+      for( var x = 0; x < _this.axis.length; x++ ) {
+        var comparison = dtw.compute(_this.baseData[_this.axis[x]], _this.data[_this.axis[x]]);
+        document.querySelector('#comparison-' + _this.axis[x] + ' span').innerHTML = comparison;
+      }
+    },
+    randomColorGenerator: function () { 
+      return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+    },
+  };
 });
+
 
 Template.accelerometer.helpers({
 /*
@@ -96,12 +143,33 @@ Template.accelerometer.helpers({
 })
 
 Template.accelerometer.events({
+  'change input[type="checkbox"]': function() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked'); 
+    var checkedAxis = [];
+    for(var i = 0; i < checkboxes.length; i++) {
+      checkedAxis.push(checkboxes[i].value)
+    }
+
+    accel.axis = checkedAxis;
+  },
+  'click #accel-base-start': function () {
+    // Bind event
+    if (window.DeviceMotionEvent) {
+      window.addEventListener('devicemotion', accel.pushRecord, false);
+    }
+    accel.start();
+  },
+  'click #accel-base-stop': function () {
+    // Unbind event
+    window.removeEventListener('devicemotion', accel.pushRecord );
+
+    accel.endBase();
+  },
   'click #accel-start': function () {
     // Bind event
     if (window.DeviceMotionEvent) {
       window.addEventListener('devicemotion', accel.pushRecord, false);
     }
-
     accel.start();
   },
   'click #accel-stop': function () {
@@ -109,5 +177,8 @@ Template.accelerometer.events({
     window.removeEventListener('devicemotion', accel.pushRecord );
 
     accel.end();
+  },
+  'click #compare': function () {
+    accel.compare();
   },
 })
