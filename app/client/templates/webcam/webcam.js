27146e0d3da65203dd9c8cb9dd2c1a9e68164af6
cas <casska@gmail.com>
2015-08-05 14:43:10 -0500

    add webcam take pic, analyse it, check for color matching

--- app/client/templates/webcam/webcam.js
+++ app/client/templates/webcam/webcam.js
@@ -1,30 +1,62 @@
 Template.webcam.onRendered(function () {
+  // A ver
+  //
 });
 
 Template.webcam.onCreated( function () {
+  // A ver
+
 });
 
 
 Template.webcam.helpers({
   isLoggedIn: function () {
     return !!Meteor.user();
-  }
-})
+  },
+});
 
 Template.webcam.events({
   'click #start-webcam': function () {
     MeteorCamera.getPicture({
       width: 640,
       height: 480,
-      quality: 80
+      quality: 80,
     }, function (err, data) {
       if(err) { 
         console.log(err);
       } else {
         console.log(data);
-        document.getElementById('webcam-img').src = data; 
+        var webcamImage = document.getElementById('webcam-img');
+
+        webcamImage.src = data; 
+
+        var colorThief = new ColorThief.colorRob();
+
+        paletteArray = colorThief.getPalette(webcamImage,2);
+        console.log(paletteArray);
+
+        var result = false;
+
+        for(var i = 0; i < paletteArray.length; i++) {
+          if( isNeighborColor([255, 51, 153,], paletteArray[i], 88) ) {
+            result = true;
+            break;
+          }
+        }
+
+        console.log(result);
+        document.getElementById('result').innerHTML = result;
       }
     });
-  } 
+  }, 
 });
 
+function isNeighborColor(color1, color2, tolerance) {
+  if(tolerance == undefined) {
+    tolerance = 32;
+  }
+
+  return Math.abs(color1[0] - color2[0]) <= tolerance
+  && Math.abs(color1[1] - color2[1]) <= tolerance
+  && Math.abs(color1[2] - color2[2]) <= tolerance;
+}
