4b6a27aeeba5007349f98638c5e1f17060e1e998
mikey <michaelrayvon@gmail.com>
2015-08-20 19:16:31 -0500

    add animation router

---
+++ app/client/helpers/router-animation.js
@@ -0,0 +1,24 @@
+/* ---------------------------------------------------- +/
+
+## Client Router ##
+
+Client-side Router.
+
+/+ ---------------------------------------------------- */
+
+
+// Routes
+
+Router.map(function() {
+
+  // Animation
+  
+  this.route('angry', {
+    path: '/angry'
+  });
+
+  this.route('blur', {
+    path: '/blur'
+  });
+
+});
